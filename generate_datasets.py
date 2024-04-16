6d2422c4a9b64e18fe34fe676d876b76fc340a67
lposani <lorenzo.posani@gmail.com>
2024-04-16 15:30:06 -0400

    Added decodanda analysis notebook.

--- generate_datasets.py
+++ generate_datasets.py
@@ -15,9 +15,11 @@ labelsB = (choice + 1) / 2
 session_VIS = generate_synthetic_data(n_neurons=200, n_trials=100,
                                       timebins_per_trial=1,
                                       mixing_factor=0.5,
-                                      rateA=0.5,
+                                      rateA=0.6,
                                       rateB=0.0,
                                       labelsA=labelsA, labelsB=labelsB)
+dec = Decodanda(session_VIS, {'stimulus': [-1, 1], 'action': [-1, 1]}, verbose=False)
+visualize_PCA(dec, mean=True, names=['A>', 'A<', 'B>', 'B<'], ndata=100)
 
 pickle.dump(session_VIS, open('./datasets/VISp.pck', 'wb'))
 
@@ -37,10 +39,20 @@ pickle.dump(session_MO, open('./datasets/MOp.pck', 'wb'))
 session_HPC = generate_synthetic_data(n_neurons=200, n_trials=100,
                                       timebins_per_trial=1,
                                       mixing_factor=0.5,
-                                      rateA=0.3,
-                                      rateB=0.3,
+                                      rateA=0.35,
+                                      rateB=0.35,
                                       labelsA=labelsA, labelsB=labelsB)
+dec = Decodanda(session_HPC, {'stimulus': [-1, 1], 'action': [-1, 1]}, verbose=False)
 
+visualize_PCA(dec, mean=True, names=['A>', 'A<', 'B>', 'B<'], ndata=200)
+plt.suptitle('Hippocampus')
+plt.savefig('./img/hpc_pca.png')
+
+f, axs = plt.subplots(1, 2, figsize=(7, 3.5), gridspec_kw={'width_ratios': [3, 2]})
+ccgps, null_ccgps = dec.CCGP(plot=True, ax=axs[1])
+perfs, null_perfs = dec.decode(0.8, plot=True, non_semantic=True, ax=axs[0], nshuffles=4)
+plt.suptitle('Hippocampus')
+plt.savefig('./img/hpc_decoding.png')
 pickle.dump(session_HPC, open('./datasets/HPC.pck', 'wb'))
 
 # Generate PFC
